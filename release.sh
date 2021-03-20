1e6d3d0a8d218fb31eb8fbc9156d118e75966758
ocavue <ocavue@gmail.com>
2021-03-20 20:21:04 +0800

    update release script

--- release.sh
+++ release.sh
@@ -2,9 +2,11 @@ set -ex
 
 cd $(dirname $0)
 
+remirror_version=$(cat package.json | egrep -o '0.0.0-pr877[.0-9]*' | sort | uniq)
 
 echo "" > .env
 echo "VITE_GIT_COMMIT_SHA=$(git rev-parse HEAD)" >> .env
 echo "VITE_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")" >> .env
+echo "VITE_REMIRROR_VERSION=$remirror_version" >> .env
 
 vercel --prod
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import Editor from "./Editor";
 
 function App() {
   const title =
+    `remirror version: ${import.meta.env.VITE_REMIRROR_VERSION}; ` +
     `build date: ${import.meta.env.VITE_BUILD_DATE}; ` +
-    `commit sha: ${import.meta.env.VITE_BUILD_DATE}; `;
+    `commit sha: ${import.meta.env.VITE_GIT_COMMIT_SHA}; `;
   return (
     <div className="App">
       <p>
